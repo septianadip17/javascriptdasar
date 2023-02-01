e67fdc07a5440bd0fdcac06ff20f76fe2eb36dd3
septianadip17 <septianadip7998@gmail.com>
2023-02-01 09:00:11 +0700

    modif pc

--- kalkulatorterminal.js
+++ kalkulatorterminal.js
@@ -2,25 +2,25 @@ const prompt = require('prompt-sync')();
 console.log("hello world")
 console.log("ayo pilih operatormu! (+, -, /, *)")
 operator = prompt(' ');
-if (operator == "+" || operator == "-" || operator == "/" || operator == "*" ){
+if (operator == "+" || operator == "-" || operator == "/" || operator == "*") {
     console.log("sip.")
-}else {
+} else {
     console.log('Masukkin yang bener tolol')
 }
-console.log(`Ini adalah operator yang kamu pilih "${operator}"` );
+console.log(`Ini adalah operator yang kamu pilih "${operator}"`);
 console.log("lanjut, masukkan angka pertamamu: ")
 firstNumber = prompt('> ');
 console.log("lanjut, masukkan angka keduamu: ")
 secondNumber = prompt('> ');
-if  ( operator == "+" ){
-    result =  parseInt(firstNumber) + parseInt(secondNumber)
-}else if (operator == "-"){
-    result =  parseInt(firstNumber) - parseInt(secondNumber)
-}else if (operator == "/"){
-    result =  parseInt(firstNumber) / parseInt(secondNumber)
-}else if(operator == "*"){
-    result =  parseInt(firstNumber) * parseInt(secondNumber)
-}else{
-    console.log("tolol kaga jelas milih lu")
+if (operator == "+") {
+    result = parseInt(firstNumber) + parseInt(secondNumber)
+} else if (operator == "-") {
+    result = parseInt(firstNumber) - parseInt(secondNumber)
+} else if (operator == "/") {
+    result = parseInt(firstNumber) / parseInt(secondNumber)
+} else if (operator == "*") {
+    result = parseInt(firstNumber) * parseInt(secondNumber)
+} else {
+    console.log("kaga jelas milih lu")
 }
 console.log(result)
